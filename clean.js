fb123ce296c0191db8eea14fcba60a427a035ebf
kakts <kakts.git@gmail.com>
2017-06-06 23:34:02 +0900

    Fix clean.js

--- clean.js
+++ clean.js
@@ -2,32 +2,53 @@ const Twitter = require('twitter');
 const _ = require('lodash');
 const async = require('neo-async');
 const config = require('./conf/credential');
-
-console.error("hello--", config);
+const csv = require('csvtojson');
 
 // Create twitter client instance.
 const client = new Twitter(config);
 
-const params = {
-  screen_name: 'YOUR_ACCOUNT'
-};
+const screenName = 'pupjnky';
 
-let tweetsCount;
-async.series([
-  (next) => {
-    client.get('users/show', params, (err, userStatus, response) => {
+// Please set you latest date what you want to delete.
+// This script delete user tweets as descending-date-order
+// It can delete max 3200 tweets one time.
+const finalDate = "2017-04-01";
+const finalTimestamp = Date.parse(finalDate);
+
+const MAX_API_REQUEST_COUNT = 3200;
+
+let list = [];
+let deleteCount = 0;
+
+console.log('------Start deleting.-----');
+csv()
+  .fromFile('./tweets.csv')
+  .on('json',(jsonObj)=>{
+    const tweetTimestamp = Date.parse(jsonObj.timestamp);
+    if (finalTimestamp > tweetTimestamp) {
+      list.push(jsonObj.tweet_id);
+    }
+  })
+  .on('done',(error)=>{
+    async.eachSeries(list, (tweetId, next) => {
+      // If the deleteCount exceeds MAX_API_REQUEST_COUNT, it will be skipped.
+      if (deleteCount > MAX_API_REQUEST_COUNT) {
+        return next();
+      }
+      // Delete specified tweets.
+      client.post('statuses/destroy/' + tweetId, (err, result, response) => {
+        if (err) {
+          return next(err);
+        }
+        deleteCount++;
+        console.log('success: tweetId: ', tweetId, deleteCount);
+        next();
+      });
+    }, (err, res) => {
       if (err) {
         console.error(err);
-        return next(err);
+        return;
       }
-      console.error("----tweets", userStatus);
-      tweetsCount = userStatus && userStatus.statuses_count;
-      return next();
+      console.log('finish. The last delete tweetId: ' + tweetId + '\n');
     });
-  },
-  (next) => {
-    next();
-  }
-], (err) => {
-  console.error('------err', err)
-});
+  });
